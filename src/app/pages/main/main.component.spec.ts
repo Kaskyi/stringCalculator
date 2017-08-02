84bb81d5739de91b7b4c194b2f5ebabcb6b6974c
Yevhenii Kamenskyi <jkamenskiy@gmail.com>
2017-08-02 10:59:21 +0300

    feat(main): add realization

--- src/app/pages/main/main.component.spec.ts
+++ src/app/pages/main/main.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { MainComponent } from './main.component';
+import { CalculatorService } from 'app/_services/calculator.service';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -8,9 +10,11 @@ describe('MainComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      providers: [CalculatorService],
+      declarations: [MainComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, ValidatorFn, AbstractControl, FormControl } from '@angular/forms';
+import { CalculatorService } from 'app/_services/calculator.service';
+import { isCountOfRoundBracketsRight, hasOnlyMathSymbols } from 'app/_models/arithmetics';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  myInput = '';
+
+  constructor(private calculatorService: CalculatorService) { }
 
   ngOnInit() {
   }
 
+  get hasRightSymbols(): boolean {
+    return hasOnlyMathSymbols(this.myInput);
+  }
+
+  get isRoundBracketsRight(): boolean {
+    return isCountOfRoundBracketsRight(this.myInput);
+  }
+
+  get calculationResult(): string {
+    return this.calculatorService.fullExpresion;
+  }
+
+  IsRoundBracketsRight(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      const name = control.value;
+      const no = !isCountOfRoundBracketsRight(this.myInput);
+      return no ? { 'IsRoundBracketsRight': name } : null;
+    };
+  }
+
+  inputFormControl = new FormControl('', [
+    Validators.required,
+    this.IsRoundBracketsRight()
+  ]);
+
+  calculate() {
+    this.calculatorService.parseData(this.myInput);
+  }
 }
