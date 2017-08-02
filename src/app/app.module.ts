07b08519e45872f3905ecdec7e723f4325e0b314
Yevhenii Kamenskyi <jkamenskiy@gmail.com>
2017-08-02 10:25:58 +0300

    feat: add main components and  imports
    add header
    add footer
    add main
    add material styles and import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdButtonModule, MdInputModule, MdGridListModule, MdIconModule, MdCardModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './_components/header/header.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MdButtonModule,
+    MdInputModule,
+    MdGridListModule,
+    MdIconModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
